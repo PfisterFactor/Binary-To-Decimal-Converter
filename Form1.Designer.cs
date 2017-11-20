4bd43e052889af60714c267faaccfa037f98a114
EJ <EJPfister55@gmail.com>
2017-11-19 22:57:02 -0600

    Commit Files

---
+++ Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace BinaryToDecimalConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.DescriptionLabel1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.BinaryLabel = new System.Windows.Forms.Label();
+            this.DecimalLabel = new System.Windows.Forms.Label();
+            this.ConvertButton = new System.Windows.Forms.Button();
+            this.ClearButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // DescriptionLabel1
+            // 
+            this.DescriptionLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DescriptionLabel1.Location = new System.Drawing.Point(12, 9);
+            this.DescriptionLabel1.Name = "DescriptionLabel1";
+            this.DescriptionLabel1.Size = new System.Drawing.Size(302, 98);
+            this.DescriptionLabel1.TabIndex = 0;
+            this.DescriptionLabel1.Text = "Input a binary number or a decimal number and click \"Convert\"";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 145);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.binarytext_KeyPress);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(214, 145);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.decimaltext_KeyPress);
+            // 
+            // BinaryLabel
+            // 
+            this.BinaryLabel.AutoSize = true;
+            this.BinaryLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BinaryLabel.Location = new System.Drawing.Point(38, 124);
+            this.BinaryLabel.Name = "BinaryLabel";
+            this.BinaryLabel.Size = new System.Drawing.Size(53, 20);
+            this.BinaryLabel.TabIndex = 3;
+            this.BinaryLabel.Text = "Binary";
+            // 
+            // DecimalLabel
+            // 
+            this.DecimalLabel.AutoSize = true;
+            this.DecimalLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DecimalLabel.Location = new System.Drawing.Point(232, 122);
+            this.DecimalLabel.Name = "DecimalLabel";
+            this.DecimalLabel.Size = new System.Drawing.Size(66, 20);
+            this.DecimalLabel.TabIndex = 4;
+            this.DecimalLabel.Text = "Decimal";
+            // 
+            // ConvertButton
+            // 
+            this.ConvertButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ConvertButton.Location = new System.Drawing.Point(118, 136);
+            this.ConvertButton.Name = "ConvertButton";
+            this.ConvertButton.Size = new System.Drawing.Size(90, 34);
+            this.ConvertButton.TabIndex = 5;
+            this.ConvertButton.Text = "Convert";
+            this.ConvertButton.UseVisualStyleBackColor = true;
+            this.ConvertButton.Click += new System.EventHandler(this.ConvertButton_Click);
+            // 
+            // ClearButton
+            // 
+            this.ClearButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ClearButton.Location = new System.Drawing.Point(118, 222);
+            this.ClearButton.Name = "ClearButton";
+            this.ClearButton.Size = new System.Drawing.Size(90, 27);
+            this.ClearButton.TabIndex = 6;
+            this.ClearButton.Text = "Clear";
+            this.ClearButton.UseVisualStyleBackColor = true;
+            this.ClearButton.Click += new System.EventHandler(this.ClearButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(326, 261);
+            this.Controls.Add(this.ClearButton);
+            this.Controls.Add(this.ConvertButton);
+            this.Controls.Add(this.DecimalLabel);
+            this.Controls.Add(this.BinaryLabel);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.DescriptionLabel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Binary <-> Decimal";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label DescriptionLabel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label BinaryLabel;
+        private System.Windows.Forms.Label DecimalLabel;
+        private System.Windows.Forms.Button ConvertButton;
+        private System.Windows.Forms.Button ClearButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,123 @@
+﻿#define DEBUG
+#undef DEBUG
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BinaryToDecimalConverter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void decimaltext_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        } // Event fired on keypress while typing in decimal input box.
+
+        private void binarytext_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (((TextBox)sender).Text.Length >= 31 && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+            if (!(e.KeyChar == '0' || e.KeyChar == '1') && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+            
+        } // Event fired on keypress while typing in binary input box.
+
+        private void ClearButton_Click(object sender, EventArgs e) // Event fired on Clear button click.
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+        }
+        Int64 binaryToDecimal(string binaryAsString) // Converts a string of binary to a 32 bit integer, 64 bit int is used to prevent overflow.
+        {
+            Int64 convertedBinary = 0;
+            if (binaryAsString.Length > 63)
+            {
+                return -1;
+            }
+            int count = 0;
+            for (int i = binaryAsString.Length-1; i >= 0; i--)
+            {
+                
+                if (binaryAsString[i] == '1')
+                {
+                    convertedBinary += (Int64)Math.Pow((double)2, (double)count);
+                }
+                count++;
+            }
+                return convertedBinary;
+        }
+        string decimalToBinary(Int32 Decimal) // Converts an Int32 to binary.
+        {
+            string convertedDecimal = "";
+            Int64 editDecimal = Decimal; // We perform our operations on this to not screw up the original data.
+            for (int i = 31; i >= 0; i--)
+            {
+                Int64 TwoToTheithPower = (Int64)Math.Pow((double)2, (double)i);
+#if (DEBUG)
+                Console.WriteLine(TwoToTheithPower);
+                Console.WriteLine(editDecimal - TwoToTheithPower >= 0);
+#endif
+                if (editDecimal - TwoToTheithPower >=0)
+                {
+                    editDecimal -= TwoToTheithPower;
+                    convertedDecimal += "1";
+                }
+                else
+                {
+                    convertedDecimal += "0";
+                }
+            }
+
+            for (int i = 0; i <= convertedDecimal.Length-1; i++) // Trims the trailing zeroes to get a much cleaner (and shorter) result.
+            {
+                if (convertedDecimal[i] == '1')
+                {
+#if (DEBUG)
+                    Console.WriteLine(i);
+#endif
+                    convertedDecimal = convertedDecimal.Remove(0, i);
+                    break;
+                }
+            }
+                return convertedDecimal;
+        }
+
+        private void ConvertButton_Click(object sender, EventArgs e) // Event fired on Convert button click.
+        {
+
+            if (textBox1.Text != "")
+            {
+                Int64 convertedBinary = binaryToDecimal(textBox1.Text);
+                textBox2.Text = Convert.ToString(convertedBinary);
+                textBox1.Text = "";
+            }
+            else if (textBox2.Text != "")
+            {
+                string convertedDecimal = decimalToBinary(Convert.ToInt32(textBox2.Text));
+                textBox1.Text = convertedDecimal;
+                textBox2.Text = "";
+            }
+            
+        }
+    
+
+
+    }
+}
